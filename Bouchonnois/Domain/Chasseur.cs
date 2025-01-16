0581a755623849b21cb9df0b8b230c2ecfc155db
Alexandre Trigueros <alexandre.trigueros@changing-chameleon.com>
2025-01-16 21:39:28 +0100

    refactor: suppress some warnings

--- Bouchonnois/Domain/Chasseur.cs
+++ Bouchonnois/Domain/Chasseur.cs
@@ -2,7 +2,7 @@ namespace Bouchonnois.Domain
 {
     public class Chasseur
     {
-        public string Nom { get; set; }
+        public required string Nom { get; init; }
         public int BallesRestantes { get; set; }
         public int NbGalinettes { get; set; }
     }
--- Bouchonnois/Service/Terrain.cs
+++ Bouchonnois/Service/Terrain.cs
@@ -2,7 +2,7 @@ namespace Bouchonnois.Service
 {
     public class Terrain
     {
-        public string Nom { get; init; }
+        public required string Nom { get; init; }
         public int NbGalinettes { get; set; }
     }
 }
