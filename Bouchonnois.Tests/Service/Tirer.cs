0065aa2c03ca2006beabc3adf33ddd89744cd954
Alexandre Trigueros <alexandre.trigueros@changing-chameleon.com>
2025-01-23 22:13:41 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:Bouchonnois.Tests/Service/Tirer.cs

--- Bouchonnois.Tests/Service/Tirer.cs
+++ Bouchonnois.Tests/Service/Tirer.cs
@@ -38,18 +38,33 @@ public class Tirer
         var savedPartieDeChasse = repository.SavedPartieDeChasse();
         savedPartieDeChasse.Id.Should().Be(id);
         savedPartieDeChasse.Status.Should().Be(PartieStatus.EnCours);
-        savedPartieDeChasse.Terrain.Nom.Should().Be("Pitibon sur Sauldre");
-        savedPartieDeChasse.Terrain.NbGalinettes.Should().Be(3);
-        savedPartieDeChasse.Chasseurs.Should().HaveCount(3);
-        savedPartieDeChasse.Chasseurs[0].Nom.Should().Be("Dédé");
-        savedPartieDeChasse.Chasseurs[0].BallesRestantes.Should().Be(20);
-        savedPartieDeChasse.Chasseurs[0].NbGalinettes.Should().Be(0);
-        savedPartieDeChasse.Chasseurs[1].Nom.Should().Be("Bernard");
-        savedPartieDeChasse.Chasseurs[1].BallesRestantes.Should().Be(7);
-        savedPartieDeChasse.Chasseurs[1].NbGalinettes.Should().Be(0);
-        savedPartieDeChasse.Chasseurs[2].Nom.Should().Be("Robert");
-        savedPartieDeChasse.Chasseurs[2].BallesRestantes.Should().Be(12);
-        savedPartieDeChasse.Chasseurs[2].NbGalinettes.Should().Be(0);
+        savedPartieDeChasse.Terrain.Should().BeEquivalentTo(new Terrain()
+        {
+            Nom = "Pitibon sur Sauldre",
+            NbGalinettes = 3
+        });
+        savedPartieDeChasse.Chasseurs.Should().BeEquivalentTo(new Chasseur[]
+        {
+            new()
+            {
+                Nom = "Dédé",
+                BallesRestantes = 20,
+                NbGalinettes = 0
+            }, 
+            new()
+            {
+                Nom = "Bernard",
+                BallesRestantes = 7,
+                NbGalinettes = 0
+            },
+            new()
+            {
+                Nom = "Robert",
+                BallesRestantes = 12,
+                NbGalinettes = 0
+            }
+            
+        });
     }
 
     [Fact]
